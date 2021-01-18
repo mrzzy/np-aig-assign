1a5000dec8061ea3d4143d0b87fea43722b4b84d
Ana <anakatrina1101@gmail.com>
2021-01-18 19:21:45 +0800

    Added flee state

--- Knight_TeamA.py
+++ Knight_TeamA.py
@@ -23,10 +23,12 @@ class Knight_TeamA(Character):
         self.melee_cooldown = 2.0
 
         seeking_state = KnightStateSeeking_TeamA(self)
+        fleeing_state = KnightStateFleeing_TeamA(self)
         attacking_state = KnightStateAttacking_TeamA(self)
         ko_state = KnightStateKO_TeamA(self)
 
         self.brain.add_state(seeking_state)
+        self.brain.add_state(fleeing_state)
         self.brain.add_state(attacking_state)
         self.brain.add_state(ko_state)
 
@@ -40,7 +42,7 @@ class Knight_TeamA(Character):
 
         Character.process(self, time_passed)
 
-        level_up_stats = ["hp", "speed", "melee damage", "melee cooldown"]
+        level_up_stats = ["healing_cooldown", "speed", "melee cooldown"]
         if self.can_level_up():
             choice = randint(0, len(level_up_stats) - 1)
             self.level_up(level_up_stats[choice])
@@ -57,6 +59,9 @@ class KnightStateSeeking_TeamA(State):
         ]
 
     def do_actions(self):
+
+        if self.knight.current_hp < self.knight.max_hp:
+            self.knight.heal()
 
         self.knight.velocity = self.knight.move_target.position - self.knight.position
         if self.knight.velocity.length() > 0:
@@ -108,6 +113,86 @@ class KnightStateSeeking_TeamA(State):
             ].position
 
 
+class KnightStateFleeing_TeamA(State):
+
+    def __init__(self, knight):
+
+        State.__init__(self, "fleeing")
+        self.knight = knight
+
+        # set end nodes to be near team base
+        if self.knight.team_id == 0:
+            keys = [5, 0, 1, 2]
+        else:
+            keys = [3, 4, 7, 6]
+
+        self.possible_end_nodes = [self.knight.world.graph.nodes.get(key) for  key in keys]
+
+        # set graph to world graph
+        self.path_graph = self.knight.world.graph
+
+    def do_actions(self):
+
+        self.knight.heal()
+
+        if self.knight.move_target.position != None:
+
+            self.knight.velocity = self.knight.move_target.position - self.knight.position
+
+            if self.knight.velocity.length() > 0:
+                self.knight.velocity.normalize_ip();
+                self.knight.velocity *= self.knight.maxSpeed
+
+
+    def check_conditions(self):
+
+        if self.knight.current_hp >= self.knight.max_hp * 0.85:
+            return "seeking"
+        
+        if (self.knight.position - self.knight.move_target.position).length() < 8:
+
+            # continue on path
+            if self.current_connection < self.path_length:
+                self.knight.move_target.position = self.path[self.current_connection].toNode.position
+                self.current_connection += 1
+
+            # reached end of path
+            else:
+                nearest_opponent = self.knight.world.get_nearest_opponent(self.knight)
+
+                # move if there are enemies
+                if nearest_opponent is not None:
+                    self.getNewPath()
+            
+        return None
+
+
+    def entry_actions(self):
+        self.getNewPath()
+
+    def getNewPath(self):
+        nearest_node = self.path_graph.get_nearest_node(self.knight.position)
+
+        nodes = list(self.knight.world.graph.nodes.values())
+        random_end_node = nearest_node
+        while random_end_node.id == nearest_node.id:
+            random_end_node = self.possible_end_nodes[randint(0, 2)]
+
+        self.path = pathFindAStar(self.path_graph, \
+                                  nearest_node, \
+                                  random_end_node)
+        
+        self.path_length = len(self.path)
+
+        if (self.path_length > 0):
+            self.current_connection = 0
+            self.knight.move_target.position = self.path[0].fromNode.position
+
+        else:
+            self.knight.move_target.position = None
+
+
+
 class KnightStateAttacking_TeamA(State):
     def __init__(self, knight):
 
@@ -137,6 +222,10 @@ class KnightStateAttacking_TeamA(State):
             self.knight.target = None
             return "seeking"
 
+        if self.knight.current_hp <= self.knight.max_hp * 0.3:
+            self.knight.target = None
+            return "fleeing"
+            
         return None
 
     def entry_actions(self):
